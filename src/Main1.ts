6d3f701506d5c694a8c16d38e42d2f55aca4eca0
Scott Grodberg <scott@trwebco.com>
2024-07-31 15:45:29 -0400

    Extract scene to its own class.

--- src/Main1.ts
+++ src/Main1.ts
@@ -1,10 +1,8 @@
 
-import { BoxGeometry, Color, CylinderGeometry, Mesh, MeshBasicMaterial } from "three";
 import { CameraManMain } from "./Camera/CameraManMain";
 import { Data } from "./Data";
 import { GameEngine } from "./GameEngine";
-import { CylinderRingsMaterial } from "./Materials/CylinderRingsMaterial";
-import { Utility } from "./Utilities/Utility";
+import { CylinderOnPlane } from "./Scenes/CylinderOnPlane";
 
 // MAIN 
 console.clear();
@@ -14,35 +12,11 @@ window.addEventListener("DOMContentLoaded", async () => {
     const canvas = document.getElementById("elementDungeon") as HTMLCanvasElement;
     const data = new Data(canvas)
     const cameraManMain = new CameraManMain(data);
-    const gameEngine = new GameEngine(data, canvas, cameraManMain);
 
+    const gameEngine = new GameEngine(data, canvas, cameraManMain);
     gameEngine.init();
 
-    if (!data.camera) {
-        throw new Error(`${Utility.timestamp()} Expected camera`);
-    }
-    const ground = new Mesh(new BoxGeometry(10, 1, 10), new MeshBasicMaterial({ color: new Color(0xffffff) }));
-    data.scene.add(ground);
+    new CylinderOnPlane().go(data, cameraManMain);
 
-    // cyliner outer
-    const height = 3.0;
-    const mesh = new Mesh(new CylinderGeometry(1, 1, height), undefined);
-    mesh.position.y = 3;
-    data.scene.add(mesh);
-
-    // small box inner
-    const mesh2 = new Mesh(new BoxGeometry(3, 1, 1), new MeshBasicMaterial({ color: new Color(0x00ff00) }));
-    mesh2.position.y = 3;
-    data.scene.add(mesh2);
-
-    data.camera.position.set(0, 7, -12);
-    data.camera?.lookAt(0, 2, 0);
-
-    const shaderMat = new CylinderRingsMaterial().clone();
-    const plainMat = new MeshBasicMaterial({ color: new Color(0x0000ff) });
-    const mats = [shaderMat, plainMat, plainMat];
-    mesh.material = mats;
-
-    cameraManMain.makeCameraOrbital(mesh.position);
 });
 
---
+++ src/Scenes/CylinderOnPlane.ts
@@ -0,0 +1,37 @@
+import { BoxGeometry, Color, CylinderGeometry, Mesh, MeshBasicMaterial } from "three";
+import { CameraManMain } from "../Camera/CameraManMain";
+import { Data } from "../Data";
+import { CylinderRingsMaterial } from "../Materials/CylinderRingsMaterial";
+import { Utility } from "../Utilities/Utility";
+
+export class CylinderOnPlane {
+
+    go(data: Data, cameraManMain: CameraManMain) {
+        if (!data.camera) {
+            throw new Error(`${Utility.timestamp()} Expected camera`);
+        }
+        const ground = new Mesh(new BoxGeometry(10, 1, 10), new MeshBasicMaterial({ color: new Color(0xffffff) }));
+        data.scene.add(ground);
+
+        // cylinder outer
+        const height = 3.0;
+        const mesh = new Mesh(new CylinderGeometry(1, 1, height), undefined);
+        mesh.position.y = 3;
+        data.scene.add(mesh);
+
+        // small box inner
+        const mesh2 = new Mesh(new BoxGeometry(3, 1, 1), new MeshBasicMaterial({ color: new Color(0x00ff00) }));
+        mesh2.position.y = 3;
+        data.scene.add(mesh2);
+
+        data.camera.position.set(0, 7, -12);
+        data.camera?.lookAt(0, 2, 0);
+
+        const shaderMat = new CylinderRingsMaterial().clone();
+        const plainMat = new MeshBasicMaterial({ color: new Color(0x0000ff) });
+        const mats = [shaderMat, plainMat, plainMat];
+        mesh.material = mats;
+
+        cameraManMain.makeCameraOrbital(mesh.position);
+    }
+}
