79988651f750aaf8f2a40f205e5f6b1956faa0f4
Scott Grodberg <scott@trwebco.com>
2024-10-14 11:39:33 -0400

    ForgePrintMaterial is basically working.

--- src/Materials/ForgePrintMaterial.ts
+++ src/Materials/ForgePrintMaterial.ts
@@ -3,6 +3,7 @@ import { Clock, ShaderMaterial } from "three";
 export class ForgePrintMaterial extends ShaderMaterial {
 
     uniforms = {
+        uY: { value: 3.0 },
     };
 
     clock!: Clock;
@@ -16,11 +17,17 @@ export class ForgePrintMaterial extends ShaderMaterial {
     `;
     fragmentShader = `
         varying vec4 vPosition;
+        uniform float uY;
+
         void main(void) {
             vec3 color = vec3(0.0);
             color.r =step(0.0,vPosition.x);
             color.g =step(0.0,vPosition.y);
             color.b =step(0.0,vPosition.z);
+            
+            if (vPosition.y > uY) {
+                discard;
+            }
             gl_FragColor = vec4(color,1.0);
         }
     `;
--- src/Scenes/Forge.ts
+++ src/Scenes/Forge.ts
@@ -1,5 +1,5 @@
 import GUI from "lil-gui";
-import { BoxGeometry, Color, Mesh, MeshPhongMaterial, Object3D, PointLight } from "three";
+import { BoxGeometry, Color, DoubleSide, Mesh, MeshPhongMaterial, Object3D, PointLight } from "three";
 import { CameraManMain } from "../Camera/CameraManMain";
 import { Data } from "../Data";
 import { ForgePrintMaterial } from "../Materials/ForgePrintMaterial";
@@ -8,7 +8,10 @@ import { Utility } from "../Utilities/Utility";
 
 export class Forge {
 
+    SPEED = 0.02;  // per tick
+
     shaderMat?: ForgePrintMaterial;
+    interval?: number;
 
     go(data: Data, cameraManMain: CameraManMain) {
         if (!data.camera) {
@@ -27,24 +30,34 @@ export class Forge {
         const geometryLeg = new BoxGeometry(0.5, 2.0, 0.5);
         const meshLeg = new Mesh(geometryLeg, undefined);
         const meshLeftLeg = meshLeg.clone();
-        meshLeftLeg.position.x += 0.4;
+        meshLeftLeg.position.set(0.4, 1.5, 0.0);
         meshLeftLeg.rotateZ(Math.PI / 16);
         const meshRightLeg = meshLeg.clone();
-        meshRightLeg.position.x -= 0.4;
+        meshRightLeg.position.set(-0.4, 1.5, 0.0);
         meshRightLeg.rotateZ(Math.PI / -16);
         const object = new Object3D();
         object.add(meshLeftLeg, meshRightLeg);
-        object.position.y = 3;
         data.scene.add(object);
 
-        data.camera.position.set(0, 7, -12);
-        data.camera?.lookAt(0, 2, 0);
+        data.camera.position.set(0, 5, -5);
+        data.camera?.lookAt(0.0, 1.0, 0.0);
 
         this.shaderMat = new ForgePrintMaterial().clone();
+        this.shaderMat.side = DoubleSide;
         meshLeftLeg.material = this.shaderMat;
         meshRightLeg.material = this.shaderMat;
+
+        gui.add(this, "SPEED", 0.01, 0.07, 0.01).name("distance per tick");
+        gui.add(this, "print");
 
         cameraManMain.makeCameraOrbital(object.position);
     }
 
+    print() {
+        clearInterval(this.interval);
+        this.shaderMat!.uniforms.uY.value = 0.0;
+        this.interval = setInterval(() => {
+            this.shaderMat!.uniforms.uY.value += this.SPEED;
+        }, 16.6);
+    }
 }
