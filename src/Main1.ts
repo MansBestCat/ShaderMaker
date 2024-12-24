22350d11aff05b63bab22792c5f9d49d444679fa
Scott Grodberg <scott@trwebco.com>
2024-12-24 16:05:01 -0500

    Introduce an additional html page to display the desired scene and put routing logic into Main1.ts

--- src/Main1.ts
+++ src/Main1.ts
@@ -2,7 +2,13 @@
 import { CameraManMain } from "./Camera/CameraManMain";
 import { Data } from "./Data";
 import { GameEngine } from "./GameEngine";
+import { CylinderOnPlane } from "./Scenes/CylinderOnPlane";
+import { CylinderOnPlane2 } from "./Scenes/CylinderOnPlane2";
+import { DropZone } from "./Scenes/DropZone";
 import { Forge } from "./Scenes/Forge";
+import { PlaneShockWave } from "./Scenes/PlaneShockWave";
+import { PlaneShockWavePulse } from "./Scenes/PlaneShockWavePulse";
+import { TubePulseOnPlane } from "./Scenes/TubePulseOnPlane";
 
 // MAIN 
 console.clear();
@@ -16,13 +22,29 @@ window.addEventListener("DOMContentLoaded", async () => {
     const gameEngine = new GameEngine(data, canvas, cameraManMain);
     gameEngine.init();
 
-    //new CylinderOnPlane().go(data, cameraManMain);
-    //new CylinderOnPlane2().go(data, cameraManMain);
-    //new PlaneShockWave().go(data, cameraManMain);
-    //new PlaneShockWavePulse().go(data, cameraManMain);
-    //new TubePulseOnPlane().go(data, cameraManMain);
-    //new DropZone().go(data, cameraManMain);
-    new Forge().go(data, cameraManMain);
+    switch (window.location.hash.substring(1)) {
+        case "CylinderOnPlane":
+            new CylinderOnPlane().go(data, cameraManMain);
+            break;
+        case "CylinderOnPlane2":
+            new CylinderOnPlane2().go(data, cameraManMain);
+            break;
+        case "DropZone":
+            new DropZone().go(data, cameraManMain);
+            break;
+        case "Forge":
+            new Forge().go(data, cameraManMain);
+            break;
+        case "PlaneShockWave":
+            new PlaneShockWave().go(data, cameraManMain);
+            break;
+        case "PlaneShockWavePulse":
+            new PlaneShockWavePulse().go(data, cameraManMain);
+            break;
+        case "TubePulseOnPlane":
+            new TubePulseOnPlane().go(data, cameraManMain);
+            break;
+    }
 
 });
 
