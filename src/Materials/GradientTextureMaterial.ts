0ef938905838c76272bb24764070660d6b3a6ee0
Scott Grodberg <scott@trwebco.com>
2025-02-17 11:23:28 +0700

    Code for starting and ending color stops for the gradient.

--- src/Materials/GradientTextureMaterial.ts
+++ src/Materials/GradientTextureMaterial.ts
@@ -1,33 +1,36 @@
-import { Clock, ShaderMaterial, TextureLoader } from "three";
+import { Clock, ShaderMaterial, TextureLoader, Vector3 } from "three";
 
 export class GradientTextureMaterial extends ShaderMaterial {
     uniforms = {
-        uDistance: { value: 0.0 },
+        uStartY: { value: 0.4 },  /*  TODO: Validate. uv.y value, range from 0 to 1 */
+        uEndY: { value: 0.6 },   /* uv.y value, range from 0 to 1 */
+        uStartColor: { value: new Vector3(1.0, 0.0, 0.0) },
+        uEndColor: { value: new Vector3(1.0, 1.0, 0.0) },
         uTexture: { value: new TextureLoader().load(`http://localhost/textures/crystal.jpg`) }
-
     };
     clock!: Clock;
     vertexShader = `
-        varying vec3 vPosition;
         varying vec2 vUv;
 
         void main() {
-            vPosition = position;
             vUv = uv;
             gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
         }
     `;
 
     fragmentShader = `   
-        uniform float uDistance;
+        uniform float uStartY;
+        uniform float uEndY;
+        uniform vec3 uStartColor;
+        uniform vec3 uEndColor;
         uniform sampler2D uTexture;
-        varying vec3 vPosition;
         varying vec2 vUv;
 
         void main(void) {
-            vec3 gradColor = mix(vec3(1.0,1.0,1.0), vec3(0.0,0.0,1.0), vUv.y);
+            float value = smoothstep(uStartY, uEndY, vUv.y);
+            vec3 gradColor = mix(uStartColor, uEndColor, value);
             vec3 texColor = texture2D(uTexture, vUv).rgb;
-            gl_FragColor = vec4(gradColor * texColor , 1.0);
+            gl_FragColor = vec4(gradColor * texColor, 1.0);
         }
     `;
 
