22d7d0d99e6dc5916e70c070ee84698220d0b82f
Scott Grodberg <scott@trwebco.com>
2025-02-17 17:59:46 +0700

    Fade from base to black.

--- src/Materials/GradientTextureMaterial.ts
+++ src/Materials/GradientTextureMaterial.ts
@@ -2,10 +2,10 @@ import { Clock, ShaderMaterial, TextureLoader, Vector3 } from "three";
 
 export class GradientTextureMaterial extends ShaderMaterial {
     uniforms = {
-        uStartY: { value: -1.0 },  /*  TODO: Validate start l.t. end. uv.y value, range from 0 to 1 */
-        uEndY: { value: 2.0 },   /* uv.y value, range from 0 to 1 */
+        uStartY: { value: -2.5 },  /*  TODO: Validate start l.t. end. uv.y value, range from 0 to 1 */
+        uEndY: { value: 1.0 },   /* uv.y value, range from 0 to 1 */
         uStartColor: { value: new Vector3(1.0, 0.0, 0.0) },
-        uEndColor: { value: new Vector3(1.0, 1.0, 0.0) },
+        uEndColor: { value: new Vector3(0.0, 0.0, 0.0) },
         uTexture: { value: new TextureLoader().load(`http://localhost/textures/crystal.jpg`) }
     };
     clock!: Clock;
