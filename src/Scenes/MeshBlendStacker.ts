032620cdb26cf3e72e6430bf4822305507cf4428
Scott Grodberg <scott@mansbestcat.com>
2025-12-10 10:26:16 +0700

    Two meshes and materials, bottom and top.

--- src/Scenes/MeshBlendStacker.ts
+++ src/Scenes/MeshBlendStacker.ts
@@ -1,5 +1,5 @@
 import GUI from "lil-gui";
-import { BoxGeometry, Color, Mesh, MeshBasicMaterial, MeshPhongMaterial, PointLight } from "three";
+import { AmbientLight, BoxGeometry, Color, Mesh, MeshBasicMaterial, MeshPhongMaterial, PlaneGeometry, PointLight } from "three";
 import { CameraManMain } from "../Camera/CameraManMain";
 import { Data } from "../Data";
 import { Utility } from "../Utilities/Utility";
@@ -7,7 +7,9 @@ import { Utility } from "../Utilities/Utility";
 export class MeshBlendStacker {
 
     interval?: number;
-    material: any;
+
+    materialBottom!: MeshBasicMaterial;
+    materialTop!: MeshBasicMaterial;
 
     go(data: Data, cameraManMain: CameraManMain) {
         if (!data.camera) {
@@ -18,6 +20,9 @@ export class MeshBlendStacker {
         pointLight.position.set(0, 5, -3);
         data.scene.add(pointLight);
 
+        const ambientLight = new AmbientLight("white", 0.1);
+        data.scene.add(ambientLight);
+
         const gui = new GUI();
         gui.domElement.onpointermove = (event: PointerEvent) => {
             event.stopPropagation();
@@ -26,13 +31,21 @@ export class MeshBlendStacker {
         const ground = new Mesh(new BoxGeometry(10, 1, 10), new MeshPhongMaterial({ color: new Color(0xffffff) }));
         data.scene.add(ground);
 
-        const mesh = new Mesh(new BoxGeometry(2.0, 2.0, 2.0), undefined);
-        data.scene.add(mesh);
+        this.materialBottom = new MeshBasicMaterial({ color: "red" });
+        const meshBottom = new Mesh(new PlaneGeometry(2.0, 2.0), this.materialBottom);
+        meshBottom.position.set(0.0, 3.0, 0.0);
+        meshBottom.rotateY(Math.PI);
+        data.scene.add(meshBottom);
+
+        this.materialTop = new MeshBasicMaterial({ color: "blue" });
+        const meshTop = new Mesh(new PlaneGeometry(2.0, 2.0), this.materialTop);
+        meshTop.position.set(-0.2, 2.8, -0.5);
+        meshTop.rotateY(Math.PI);
+        data.scene.add(meshTop);
 
         data.camera.position.set(0, 3, -12);
         data.camera?.lookAt(0, 3, 0);
 
-        this.material = new MeshBasicMaterial();
 
         // // this.material.uniforms.uAttackLineLength.value = this.TUBE_LENGTH;     
         // // gui.add(this.material.uniforms.uPulseLength, "value", 0.0, 4.0, 0.1).name("pulse length");
@@ -48,9 +61,9 @@ export class MeshBlendStacker {
         // gui.add(this.material.uniforms.uThickness, "value", 0.01, 0.07, 0.01).name("uThickness");
         // gui.add(this.material.uniforms.uFresnelFalloff, "value", 0.001, 1.0, 0.001).name("uFresnelFalloff");
 
-        mesh.material = this.material;
+        meshTop.material = this.materialTop;
 
-        cameraManMain.makeCameraOrbital(mesh.position);
+        cameraManMain.makeCameraOrbital(meshTop.position);
     }
 
 
